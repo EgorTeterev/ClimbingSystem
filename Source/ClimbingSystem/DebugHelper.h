841c987194dc46ec1af859072642095b667ceab6
EgorTeterev <110204072+EgorTeterev@users.noreply.github.com>
2025-07-03 16:02:25 +0300

    prj setup, Custom Movement Component

---
+++ Source/ClimbingSystem/DebugHelper.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Debug
+{
+	static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 Key = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(Key, 5.0f, Color, Msg);
+		}
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+	}
+}
